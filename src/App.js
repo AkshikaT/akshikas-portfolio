793cbc8e3585d0ab65fc2d9cf86b922958268599
Akshika Thiyagendran <takshika2005@gmail.com>
2025-01-02 09:05:27 -0500

    changed file path again

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Contact from './Pages/Contact.js';
-import Projects from '/Users/akshika/Desktop/GitHub/2025-akshika-web/akshikas-website/src/Pages/projects.js';
+import Projects from './Pages/Projects.js';
 
 /* This is the main App screen */
 function App() {
